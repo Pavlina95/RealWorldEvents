548f3b7d81ac0490a446f3bf0de42cc20dfc243a
Pavlina95 <pandonova95@gmail.com>
2020-04-18 11:09:49 +0300

    Setup esling and prettier. Created several view and components. Added routing.

---
+++ src/views/EventList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Events List:</h1>
+    <!-- <router-link :to="{ name: 'event-show', params: { id: 'Pavlina' } }">
+      Show Pavlina's Event
+    </router-link> -->
+    <EventCard />
+  </div>
+</template>
+
+<script>
+import EventCard from '../components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
