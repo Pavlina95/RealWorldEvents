b07fbce550ba9799eb234fa20f0f3357db00050c
Pavlina95 <pandonova95@gmail.com>
2020-04-19 15:37:34 +0300

    Integrated progress bar.

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import EventShow from '../views/EventShow.vue'
 import EventList from '../views/EventList.vue'
 import EventCreate from '@/views/EventCreate.vue'
 import NotFound from '@/views/NotFound'
+import NProgress from 'nprogress'
+import store from '@/store/index.js'
 
 Vue.use(VueRouter)
 
@@ -11,13 +13,20 @@ const routes = [
   {
     path: '/',
     name: 'event-list',
-    component: EventList
+    component: EventList,
+    props: true
   },
   {
     path: '/event/:id',
     name: 'event-show',
     component: EventShow,
-    props: true
+    props: true,
+    beforeEnter(routeTo, routeFrom, next) {
+      store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
+        routeTo.params.event = event
+        next()
+      })
+    }
   },
   {
     path: 'event-create',
@@ -33,6 +42,17 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+router.beforeEach((routeTo, routeFrom, next) => {
+  // Start the route progress bar.
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  // Complete the animation of the route progress bar.
+  NProgress.done()
 })
 
 export default router
--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -5,7 +5,8 @@ export const namespaced = true
 export const state = {
   events: [],
   eventsTotal: 0,
-  event: {}
+  event: {},
+  perPage: 3
 }
 
 export const mutations = {
@@ -45,8 +46,8 @@ export const actions = {
         throw error
       })
   },
-  fetchEvents({ commit, dispatch }, { perPage, page }) {
-    EventService.getEvents(perPage, page)
+  fetchEvents({ commit, dispatch, state }, { page }) {
+    return EventService.getEvents(state.perPage, page)
       .then(response => {
         commit('SET_EVENTS', response.data)
         commit('SET_EVENTS_COUNT', response.headers['x-total-count'])
@@ -65,10 +66,12 @@ export const actions = {
 
     if (event) {
       commit('SET_EVENT', event)
+      return event
     } else {
-      EventService.getEvent(id)
+      return EventService.getEvent(id)
         .then(response => {
           commit('SET_EVENT', response.data)
+          return response.data
         })
         .catch(error => {
           const notification = {
